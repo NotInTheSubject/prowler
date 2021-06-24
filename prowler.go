dc7b3bcd1f6cd7b459365f809e8e27fdd49b1116
Sergey Boldyrev <boldyrev@neurus.ru>
2021-06-24 06:18:02 +0000

    prowler.go: is updated

--- prowler.go
+++ prowler.go
@@ -1,6 +1,8 @@
 package prowler
 
 import (
+	"net/http"
+
 	"github.com/NotInTheSubject/prowler/mods"
 	"github.com/NotInTheSubject/prowler/processing"
 	"github.com/sirupsen/logrus"
@@ -14,7 +16,7 @@ type IdentifiedRequest = processing.IdentifiedRequest
 
 type FuzzingStatistic = processing.FuzzingStatistic
 
-type RequestModifier = mods.Operator
+type RequestModifier = mods.Modifier
 
 type StopCondition = processing.StopCondition
 
@@ -22,6 +24,10 @@ type SequenceProducer = processing.SequenceProducer
 
 type ExternalSystem = processing.ExternalSystem
 
-func RunProwling(log *logrus.Logger, es ExternalSystem, mods RequestModifier, stopCondition StopCondition) error {
-	return processing.RunProwling(log, es, mods, stopCondition)
+func RunProwling(log *logrus.Logger, es ExternalSystem, client http.Client, mods []RequestModifier, stopCondition StopCondition) error {
+	return processing.RunProwling(log, es, client, mods, stopCondition)
+}
+
+func DefaultModifiers() []mods.Modifier {
+	return mods.DefaultModifiers()
 }
